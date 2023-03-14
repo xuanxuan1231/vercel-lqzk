ab9cb8d3fa8644b28119742772d812ce45db06ac
chenwenxuan <1763516926@qq.com>
2023-03-14 21:36:45 +0800

    new

--- admin/login.php
+++ admin/login.php
@@ -1,57 +1,23 @@
 <?php
 include 'common.php';
-
 if ($user->hasLogin()) {
     $response->redirect($options->adminUrl);
 }
-$rememberName = htmlspecialchars(\Typecho\Cookie::get('__typecho_remember_name', ''));
-\Typecho\Cookie::delete('__typecho_remember_name');
+$sys_protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
+$path_info = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+$relate_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $php_self.(isset($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : $path_info);
+$url = urlencode($sys_protocal.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '').$relate_url);
+$url = Typecho_Common::url('user/login', Helper::options()->index).'?from='.$url;
+?>
 
-$bodyClass = 'body-100';
+<HTML>
 
-include 'header.php';
-?>
-<div class="typecho-login-wrap">
-    <div class="typecho-login">
-        <h1><a href="http://typecho.org" class="i-logo">Typecho</a></h1>
-        <form action="<?php $options->loginAction(); ?>" method="post" name="login" role="form">
-            <p>
-                <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
-                <input type="text" id="name" name="name" value="<?php echo $rememberName; ?>" placeholder="<?php _e('用户名'); ?>" class="text-l w-100" autofocus />
-            </p>
-            <p>
-                <label for="password" class="sr-only"><?php _e('密码'); ?></label>
-                <input type="password" id="password" name="password" class="text-l w-100" placeholder="<?php _e('密码'); ?>" />
-            </p>
-            <p class="submit">
-                <button type="submit" class="btn btn-l w-100 primary"><?php _e('登录'); ?></button>
-                <input type="hidden" name="referer" value="<?php echo htmlspecialchars($request->get('referer') ?? ''); ?>" />
-            </p>
-            <p>
-                <label for="remember">
-                    <input<?php if(\Typecho\Cookie::get('__typecho_remember_remember')): ?> checked<?php endif; ?> type="checkbox" name="remember" class="checkbox" value="1" id="remember" /> <?php _e('下次自动登录'); ?>
-                </label>
-            </p>
-        </form>
-        
-        <p class="more-link">
-            <a href="<?php $options->siteUrl(); ?>"><?php _e('返回首页'); ?></a>
-            <?php if($options->allowRegister): ?>
-            &bull;
-            <a href="<?php $options->registerUrl(); ?>"><?php _e('用户注册'); ?></a>
-            <?php endif; ?>
-            <a href ="<?php GmLogin_Plugin::url('login'); //输出登陆url ?>"><?php _e('新登陆'); ?></a>
-        </p>
-    </div>
-</div>
-<?php 
-include 'common-js.php';
-?>
-<script>
-$(document).ready(function () {
-    $('#name').focus();
-});
-</script>
-<?php
-include 'footer.php';
-?>
+<head>
+
+    <meta HTTP-EQUIV="REFRESH" CONTENT="3; URL='<?php echo $url; ?>' ">
+
+</head>
+
+<body>
+
+</body>
--- admin/register.php
+++ admin/register.php
@@ -1,50 +1,23 @@
 <?php
 include 'common.php';
-
 if ($user->hasLogin() || !$options->allowRegister) {
     $response->redirect($options->siteUrl);
 }
-$rememberName = htmlspecialchars(\Typecho\Cookie::get('__typecho_remember_name'));
-$rememberMail = htmlspecialchars(\Typecho\Cookie::get('__typecho_remember_mail'));
-\Typecho\Cookie::delete('__typecho_remember_name');
-\Typecho\Cookie::delete('__typecho_remember_mail');
+$sys_protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
+$path_info = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+$relate_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $php_self.(isset($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : $path_info);
+$url = urlencode($sys_protocal.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '').$relate_url);
+$url = Typecho_Common::url('user/register', Helper::options()->index).'?from='.$url;
+?>
 
-$bodyClass = 'body-100';
+<HTML>
 
-include 'header.php';
-?>
-<div class="typecho-login-wrap">
-    <div class="typecho-login">
-        <h1><a href="http://typecho.org" class="i-logo">Typecho</a></h1>
-        <form action="<?php $options->registerAction(); ?>" method="post" name="register" role="form">
-            <p>
-                <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
-                <input type="text" id="name" name="name" placeholder="<?php _e('用户名'); ?>" value="<?php echo $rememberName; ?>" class="text-l w-100" autofocus />
-            </p>
-            <p>
-                <label for="mail" class="sr-only"><?php _e('Email'); ?></label>
-                <input type="email" id="mail" name="mail" placeholder="<?php _e('Email'); ?>" value="<?php echo $rememberMail; ?>" class="text-l w-100" />
-            </p>
-            <p class="submit">
-                <button type="submit" class="btn btn-l w-100 primary"><?php _e('注册'); ?></button>
-            </p>
-        </form>
-        
-        <p class="more-link">
-            <a href="<?php $options->siteUrl(); ?>"><?php _e('返回首页'); ?></a>
-            &bull;
-            <a href="<?php $options->adminUrl('login.php'); ?>"><?php _e('用户登录'); ?></a>
-        </p>
-    </div>
-</div>
-<?php 
-include 'common-js.php';
-?>
-<script>
-$(document).ready(function () {
-    $('#name').focus();
-});
-</script>
-<?php
-include 'footer.php';
-?>
+<head>
+
+    <meta HTTP-EQUIV="REFRESH" CONTENT="3; URL='<?php echo $url; ?>' ">
+
+</head>
+
+<body>
+
+</body>
